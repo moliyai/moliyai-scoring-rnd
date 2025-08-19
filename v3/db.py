ce9a5bf53d03a712729d82f6b4d66bce740e63fa
Jasurkhon <jasur.uzb10@gmail.com>
2025-08-19 22:34:05 +0500

    RnD

--- v3/db.py
+++ v3/db.py
@@ -4,7 +4,8 @@ import os
 
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-MODEL_PATH = os.path.join(BASE_DIR, "source", "model", "merged_random_forest_model.pkl")
+# MODEL_PATH = os.path.join(BASE_DIR, "source", "model", "merged_random_forest_model.pkl")
+MODEL_PATH = os.path.join(BASE_DIR, "source", "model", "6month_random_forest_model.pkl")
 JOB_PATH = os.path.join(BASE_DIR, "source", "dataset", "jobs.csv")
 
 model = joblib.load(MODEL_PATH)
--- v3/main.py
+++ v3/main.py
@@ -11,6 +11,12 @@ from starlette.status import HTTP_303_SEE_OTHER
 from urllib.parse import urlencode
 from fastapi.staticfiles import StaticFiles
 
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+
 
 
 
@@ -18,73 +24,100 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
  
-
-USER_INPUT_CSV = "user_inputs.csv"
-
-
-def get_maximum_available_amount(salary, total_client_family_salary, client_monthly_expenditure):
-    max_available_amount = ((salary + total_client_family_salary - client_monthly_expenditure) / 2) * 12
-    return max_available_amount
-
+USER_INPUT_CSV = "predicted_client_status.csv"
 
 def get_prediction(model, data, threshold = 0):
     input_data = pd.DataFrame([data])
 
-    salary = input_data["salary"].iloc[0]
-    total_client_family_salary = input_data["total_client_family_salary"].iloc[0]
-    client_monthly_expenditure = input_data["client_monthly_expenditure"].iloc[0]
-   
-    probability = model.predict_proba(input_data)
-    prediction = model.predict(input_data)
+    shaped_bread_prices = {
+        'фаргона вилояти': 2650,
+        'наманган вилояти': 2700,
+        'андижон вилояти': 2700,
+        'тошкент шахри': 2800,
+        'тошкент вилояти': 2800
+    }
 
-    status = prediction[0]
-    input_data['status'] = status
+    cottonseed_oil_prices = {
+        'фаргона вилояти': 19750,
+        'наманган вилояти': 17100,
+        'андижон вилояти': 18500,
+        'тошкент шахри': 18250,
+        'тошкент вилояти': 18250
+    }
+
+    beef_prices = {
+        'фаргона вилояти': 77000,
+        'наманган вилояти': 76000,
+        'андижон вилояти': 82500,
+        'тошкент шахри': 85000,
+        'тошкент вилояти': 85000
+    }
+
+    feature_cols = [
+        'семейное положение', 'сумма_выдачи(usd)', 'процентная ставка',
+        'срок кредита (месяц)', 'цикл', 'цель кредита', 'возраст', 'пол',
+        'область \город', 'регион \город', 'количество членов семьи', 'образование',
+        'должность', 'shaped_bread(som)', 'cottonseed oil(som)', 'beef(som)'
+    ]
+
+    categorical_cols = [
+        'семейное положение', 'цель кредита', 'пол', 'область \город',
+        'регион \город', 'образование', 'должность'
+    ]
+
+    input_data['shaped_bread(som)'] = input_data['область \\город'].map(shaped_bread_prices)
+    input_data['cottonseed oil(som)'] = input_data['область \\город'].map(cottonseed_oil_prices)
+    input_data['beef(som)'] = input_data['область \\город'].map(beef_prices)
 
 
-    cancelled_prob = probability[0][0]
-    approved_prob = probability[0][1]
+    df_encoded = input_data.copy()
+    label_encoders = {}
+
+
+    for col in categorical_cols:
+        le = LabelEncoder()
+        df_encoded[col] = le.fit_transform(df_encoded[col].astype(str))
+        label_encoders[col] = le
+
+    X = df_encoded[feature_cols]
+
+    #вероятность класса "1" (плохой клиент)
+    y_proba_bad = model.predict_proba(X)[:, 1][0]
+    y_proba_good = 1 - y_proba_bad
+
+    bad_prob = y_proba_bad * 100
+    good_prob = y_proba_good * 100
+
+    status = "Approved" if y_proba_bad < threshold else "Not Approved"
+    
+    if status == "Approved":
+        client_status = 0
+    else:
+        client_status = 1
+    
+    input_data['predicted_client_status'] = client_status
     
 
-    if approved_prob >= threshold:
-        maximum_available_amount = int(round(approved_prob, 2) * get_maximum_available_amount(salary, total_client_family_salary, client_monthly_expenditure))
-        if maximum_available_amount <= 0:
-            status = 0
-            status_text = "not approved"
-            maximum_available_amount = 0
-        else:
-            status = 1  
-            status_text = "approved"
-            maximum_available_amount = maximum_available_amount
-    else:
-        status = 0 
-        status_text = "not approved"
-        maximum_available_amount = 0
-
-
-    input_data["max_available_amount_year"] = maximum_available_amount
     if not os.path.exists(USER_INPUT_CSV):
         input_data.to_csv(USER_INPUT_CSV, index=False)
     else:
         input_data.to_csv(USER_INPUT_CSV, mode='a', header=False, index=False)
-
-
+    
     return {
-        "prediction_status": status_text,
-        "not approved %": round(cancelled_prob*100, 2),
-        "approved %": round(approved_prob*100, 2),
-        "threshold": threshold,
-        "maximum_available_amount": maximum_available_amount
+        "prediction_status": status,
+        "approved %": round(good_prob, 2),
+        "not approved %": round(bad_prob, 2)
     }
 
+
 @app.get("/")
-async def form_page(request: Request, prediction_status: str = None, approved: float = None, not_approved: float = None, maximum_available_amount: float = None):
+async def form_page(request: Request, prediction_status: str = None, approved: float = None, not_approved: float = None):
     prediction_data = None
     if prediction_status:
         prediction_data = {
             "prediction_status": prediction_status,
             "approved": approved,
             "not_approved": not_approved,
-            "maximum_available_amount": maximum_available_amount,
         }
     
     return templates.TemplateResponse("index.html", {
@@ -97,56 +130,47 @@ async def form_page(request: Request, prediction_status: str = None, approved: f
 @app.post("/v3/predict/random/forest")
 def predict_random_forest(
     request: Request,
-    sex: int = Form(...),
-    viloyat_id: int = Form(...),
-    tuman_id: int = Form(...),
-    mfy_id: int = Form(...),
-    client_type: int = Form(...),
-    job_name: str = Form(...),
-    salary: int = Form(...),
-    total_client_family_salary: int = Form(...),
-    occupation_status: int = Form(...),
-    family_status: int = Form(...),
-    family_members_count: int = Form(...),
-    home_type: int = Form(...),
-    home_owner: int = Form(...),
-    client_monthly_expenditure: int = Form(...),
-    product_price: int = Form(...),
-    final_produt_price: int = Form(...),
-    threshold: int = Form(80), 
+    marital_status: str = Form(...),
+    age: int = Form(...),
+    gender: str = Form(...),
+    region: str = Form(...),
+    city: str = Form(...),
+    family_members: int = Form(...),
+    education: str = Form(...),
+    job_position: str = Form(...),
+    loan_amount: int = Form(...),
+    interest_rate: int = Form(...),
+    loan_term: int = Form(...), #срок кредита
+    cycle: int = Form(...),
+    loan_purpose: str = Form(...),
+    threshold: int = Form(50), 
 ):
-    
+    threshold = 50
     threshold_normalized = threshold / 100.0
   
     client_data = {
-        "sex": sex,
-        "viloyat_id": viloyat_id,
-        "tuman_id": tuman_id,
-        "mfy_id": mfy_id,
-        "client_type": client_type,
-        "job_name": job_name,
-        "salary": salary,
-        "total_client_family_salary": total_client_family_salary,
-        "occupation_status": occupation_status,
-        "family_status": family_status,
-        "family_members_count": family_members_count,
-        "home_type": home_type,
-        "home_owner": home_owner,
-        "client_monthly_expenditure": client_monthly_expenditure,
-        "product_price": product_price,
-        "final_produt_price": final_produt_price,
-    }
+        'семейное положение': marital_status,
+        'сумма_выдачи(usd)': loan_amount,
+        'процентная ставка': interest_rate,
+        'срок кредита (месяц)': loan_term,
+        'цикл': cycle,
+        'цель кредита': loan_purpose,
+        'возраст': age,
+        'пол': gender,
+        'область \город': region,
+        'регион \город': city,
+        'количество членов семьи': family_members,
+        'образование': education,
+        'должность': job_position,
+}
 
     predicted_data = get_prediction(model, client_data, threshold=threshold_normalized)
-    print("PREDICAED DATA:", predicted_data['maximum_available_amount'])
 
     query_params = {
         "prediction_status": predicted_data['prediction_status'],
         "approved": predicted_data['approved %'],
         "not_approved": predicted_data['not approved %'],
-        "maximum_available_amount": predicted_data['maximum_available_amount']
     }
 
     url = str(request.url_for("form_page")) + "?" + urlencode(query_params)
-    
     return RedirectResponse(url=url, status_code=HTTP_303_SEE_OTHER)
